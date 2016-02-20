633c791973e6e15225dd060a6af4bddae46e0bfd
Travis Webb <me@traviswebb.com>
2016-02-19 20:30:19 -0500

    [test] default "abstract" DatastoreService implementation

--- test/unit/services/DatastoreService.test.js
+++ test/unit/services/DatastoreService.test.js
@@ -6,4 +6,11 @@ describe('DatastoreService', () => {
   it('should exist', () => {
     assert(global.app.api.services['DatastoreService'])
   })
+  it('should throw a "must implement in subclass" error', () => {
+    const DatastoreService = new global.app.api.services.DatastoreService()
+    assert.throws(() => DatastoreService.select(), /must be overridden/)
+    assert.throws(() => DatastoreService.insert(), /must be overridden/)
+    assert.throws(() => DatastoreService.update(), /must be overridden/)
+    assert.throws(() => DatastoreService.delete(), /must be overridden/)
+  })
 })
